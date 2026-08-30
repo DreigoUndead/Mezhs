8cb6b4b3b5dc1b4ecdb146410282ba5a7c81a8b1
DreigoUndead <dreigo.undead@gmail.com>
2026-08-30 20:04:46 +0300

    Refine command attribute defaults

--- src/Mezhs.Console/CommandAttribute.cs
+++ src/Mezhs.Console/CommandAttribute.cs
@@ -1,12 +1,13 @@
+using System.Runtime.CompilerServices;
+
 namespace Mezhs.Console;
 
 [AttributeUsage(AttributeTargets.Method)]
 public sealed class CommandAttribute : Attribute
 {
-    public CommandAttribute() { }
-    public CommandAttribute(string name) => Name = name;
+    public CommandAttribute([CallerMemberName] string name = "") => Name = name;
 
-    public string? Name { get; }
+    public string Name { get; }
     public string? Description { get; init; }
     public string? Example { get; init; }
 }
