c48414cb538f97db122f9893b58a83e3043cf0cd
DreigoUndead <dreigo.undead@gmail.com>
2026-08-31 10:43:13 +0300

    configure agent runtime messages

--- src/Mezhs.Agent.Api/Configuration/AgentOptions.cs
+++ src/Mezhs.Agent.Api/Configuration/AgentOptions.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Mezhs.Agent.Policy;
 
 namespace Mezhs.Agent.Configuration;
@@ -7,5 +8,27 @@ public sealed class AgentOptions
     public required Uri Listen { get; init; }
     public required Uri MezhsApi { get; init; }
     public required string Storage { get; init; }
+    public required AgentRuntimeMessages Messages { get; init; }
     public required IReadOnlyDictionary<string, PolicyContext> Policies { get; init; }
+}
+
+public sealed class AgentRuntimeMessages
+{
+    [Required]
+    public string? Continue { get; set; }
+
+    [Required]
+    public string? PolicyCorrection { get; set; }
+
+    [Required]
+    public string? CommandCorrection { get; set; }
+
+    [Required]
+    public string? CommandResults { get; set; }
+
+    [Required]
+    public string? ProtocolIntro { get; set; }
+
+    [Required]
+    public string? ShellContext { get; set; }
 }
