f5cfdf848595797009787c75aec8f60053bad586
DreigoUndead <dreigo.undead@gmail.com>
2026-09-15 18:06:24 +0300

    Reuse shared MEZS execution context variables

--- src/Mezhs.Executor/ExecutorEnvironment.cs
+++ src/Mezhs.Executor/ExecutorEnvironment.cs
@@ -1,16 +1,18 @@
+using Mezhs.Console;
+
 namespace Mezhs.Executor;
 
 public static class ExecutorEnvironment
 {
     public const string StorageVariable = "MEZHS_EXECUTOR_STORAGE";
-    public const string ExecutionIdVariable = "MEZHS_EXECUTION_ID";
-    public const string ParentExecutionIdVariable = "MEZHS_PARENT_EXECUTION_ID";
-    public const string ChatIdVariable = "MEZHS_CHAT_ID";
-    public const string CorrelationIdVariable = "MEZHS_CORRELATION_ID";
-    public const string SourceVariable = "MEZHS_SOURCE";
-    public const string WorkspaceVariable = "MEZHS_WORKSPACE";
-    public const string TriggerMessageIdVariable = "MEZHS_TRIGGER_MESSAGE_ID";
-    public const string CommandIndexVariable = "MEZHS_COMMAND_INDEX";
+    public const string ExecutionIdVariable = MezhsExecutionContext.ExecutionIdVariable;
+    public const string ParentExecutionIdVariable = MezhsExecutionContext.ParentExecutionIdVariable;
+    public const string ChatIdVariable = MezhsExecutionContext.ChatIdVariable;
+    public const string CorrelationIdVariable = MezhsExecutionContext.CorrelationIdVariable;
+    public const string SourceVariable = MezhsExecutionContext.SourceVariable;
+    public const string WorkspaceVariable = MezhsExecutionContext.WorkspaceVariable;
+    public const string TriggerMessageIdVariable = MezhsExecutionContext.TriggerMessageIdVariable;
+    public const string CommandIndexVariable = MezhsExecutionContext.CommandIndexVariable;
 
     public static string ResolveStoragePath(string? explicitPath = null)
     {
